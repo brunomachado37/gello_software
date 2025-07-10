4fdd976d6a8612751c8bcdc937184c93327751e9
brunomachado37 <brunomachadocarneiro37@gmail.com>
2025-07-10 09:28:06 +0200

    Ignore cameras if not saving the data

--- experiments/run_env.py
+++ experiments/run_env.py
@@ -47,14 +47,18 @@ class Args:
 def main(args):
     if args.mock:
         robot_client = PrintRobot(8, dont_print=True)
-        camera_clients = {}
     else:
+        robot_client = ZMQClientRobot(port=args.robot_port, host=args.hostname)
+
+    if args.use_save_interface:
         camera_clients = {
             # you can optionally add camera nodes here for imitation learning purposes
             "wrist": ZMQClientCamera(port=args.wrist_camera_port, host=args.hostname),
             "side": ZMQClientCamera(port=args.base_camera_port, host=args.hostname),
         }
-        robot_client = ZMQClientRobot(port=args.robot_port, host=args.hostname)
+    else:
+        camera_clients = {}
+
     env = RobotEnv(robot_client, control_rate_hz=args.hz, camera_dict=camera_clients)
 
     if args.bimanual:
--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -63,7 +63,7 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
     "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FTA7NNHW-if00-port0": DynamixelRobotConfig(
         joint_ids=(1, 2, 3, 4, 5, 6, 7),
         joint_offsets=(
-            3 * np.pi / 2,
+            -1 * np.pi / 2,
             2 * np.pi / 2,
             4 * np.pi / 2,
             2 * np.pi / 2,
@@ -72,7 +72,7 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
             3 * np.pi / 2,
         ),
         joint_signs=(1, -1, 1, 1, 1, -1, 1),
-        gripper_config=(8, 198, 156),
+        gripper_config=(8, 197, 155),
     ),
     # # Left UR
     # "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7WBEIA-if00-port0": DynamixelRobotConfig(
